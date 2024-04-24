492e2e0e1d855263613303ccaf9530b108cc82bf
vsaprykina <92681607+vsaprykina@users.noreply.github.com>
2024-04-24 15:50:18 +0300

    js

---
+++ верстка/rom.js
@@ -0,0 +1,9 @@
+const today = new Date();
+const dd = String(today.getDate()).padStart(2, '0');
+const mm = String(today.getMonth() + 1).padStart(2, '0');
+const yyyy = today.getFullYear();
+const minDate = yyyy + '-' + mm + '-' + dd;
+
+// Устанавливаем атрибут min для поля выбора даты
+const appointmentDateInput = document.getElementById('appointmentDate');
+appointmentDateInput.setAttribute('min', minDate);
